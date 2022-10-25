023151ce58ca1f058be54c6197c29fa432d29fa9
simonesanna1983 <35135396+simonesanna1983@users.noreply.github.com>
2022-10-25 15:12:03 +0200

    2 files modified

--- App.tsx
+++ App.tsx
@@ -5,29 +5,44 @@ import ChildFooter from './child-footer';
 import './style.css';
 
 export default function App() {
-  // const [parentVal, setParentVal] = React.useState(0);
+  const [parentVal, setParentVal] = React.useState(0);
 
-  const ingredients = [
+  
+  const ingredientsInitial = [
     'butter',
     'olive oil',
     'garlic',
     'aubergine',
     'bell pepper',
   ];
-  let parentVal = 0;
+
+  const [ingredients, setIngredients] = React.useState(ingredientsInitial);
+
+  // let parentVal = 0;
   console.log('App', parentVal);
 
   const checkValue = (value) => {
-    parentVal = value;
+    // parentVal = value;
 
-    // setParentVal(value);
+    setParentVal(value);
+  };
+
+  const addIng = (ing) => {
+    console.log('addIng', ing);
+    // ingredients.push(ing);
+    setIngredients([...ingredients, ing]);
+    console.log('addIng ingredients', ingredients);
   };
 
   return (
     <Fragment>
       <div className="parentWrapper">
         Parent
-        <Child callback={checkValue} ingredients={ingredients}></Child>
+        <Child
+          callback={checkValue}
+          addIngCallback={addIng}
+          ingredients={ingredients}
+        ></Child>
         <ChildFooter val={parentVal}></ChildFooter>
       </div>
     </Fragment>
--- child.tsx
+++ child.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 
 export interface props {
   ingredients: string[];
   callback: (val: number) => void;
+  addIngCallback: (ing: string) => void;
 }
 
 export default function Child(myprop: props) {
   const [val, setVal] = React.useState(0);
+
+  const [ing, setIng] = React.useState('');
+
+  const valueUseRef = React.useRef(0);
+  const inputRef = React.useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   console.log('Child');
 
@@ -17,10 +27,13 @@ export default function Child(myprop: props) {
     myprop.callback(v + 1);
   };
 
+  const onAddIng = () => {
+    myprop.addIngCallback(ing);
+  };
+
   const showIngr = (ing: string) => {
     return <li>{ing}</li>;
   };
-
   return (
     <Fragment>
       <div className="bckGreen">
@@ -38,11 +51,30 @@ export default function Child(myprop: props) {
           Increment
         </button>
         <ul>
-          {myprop.ingredients?.map((ing) =>
-            // <li key={ing}>{ing}</li>
-            showIngr(ing)
+          {myprop.ingredients?.map(
+            (ing) =>
+              // <li>{ing}</li>
+              showIngr(ing)
+            // showIngr(ing)
           )}
         </ul>
+        <input
+          onChange={(e) => {
+            setIng(e.target.value);
+          }}
+        ></input>
+        <button onClick={onAddIng}>Add new Ingredient </button>
+        <br />
+        <br />
+        <input type="text" ref={inputRef}></input>
+        {/* <button
+          onClick={() => {
+            valueUseRef.current++;
+            console.log('valueUseRef', valueUseRef.current);
+          }}
+        >
+          Change useRef{' '}
+        </button> */}
       </div>
     </Fragment>
   );
